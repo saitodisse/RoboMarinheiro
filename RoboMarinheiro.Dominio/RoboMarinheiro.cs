52a5f396fe6f295be321df0288e1ca714b9ca640
saitodisse <saitodisse@gmail.com>
2011-12-17 11:33:30 -0200

    configuracao para TDD: NUnit + Moq

---
+++ RoboMarinheiro.Dominio/RoboMarinheiro.cs
@@ -0,0 +1,10 @@
+namespace RoboMarinheiro.Dominio
+{
+    public class RoboMarinheiro
+    {
+        public string BuscarConteudo(string uri)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ RoboMarinheiro.Teste/RoboTestClass.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace RoboMarinheiro.Dominio.Teste
+{
+    [TestFixture]
+    public class RoboTestClass
+    {
+        [Test]
+        public void deve_retornar_conteudo_da_uri_passada()
+        {
+            var robo = new RoboMarinheiro();
+            string conteudo = robo.BuscarConteudo("http://www.w3schools.com/html/tryit.asp?filename=tryhtml_intro");
+            Assert.IsNotNullOrEmpty(conteudo);
+        }
+    }
+}
